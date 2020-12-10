c64da9f2565b628561f4db49509222e7cf7f9179
Elias Claeys <elias.claeys@student.romerocollege.be>
2020-12-10 14:13:42 +0100

    Add project files.

---
+++ Som/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Som
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bereken de som van de gegeven getallen tot dat de gebruiker 0 invoert
+            /*
+             */
+
+            int getal, som;
+            som = 0; 
+
+            Console.Write("Geef een getal in ");
+            getal = int.Parse(Console.ReadLine());
+
+            while(getal != 0)
+            {
+                som = som + getal;
+
+                Console.Write("Geef een getal in ");
+                 getal = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"De Som van de getallen {som}");
+            Console.ReadLine();
+
+        }
+    }
+
+}
